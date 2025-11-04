a73e43d92517fb1da21cb9349fc71176fb1412f3
LorenzoSSAW <piccininlorenzo165@gmail.com>
2025-11-04 22:20:15 +0100

    Fix duplicato path require

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const { Pool } = require('pg');
-const path = require('path');
 
 const app = express();
 app.use(express.json());
